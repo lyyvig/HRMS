619069506539947486024039de72d3e2ccd18fd0
lyyvig <lyyvig@gmail.com>
2022-07-29 17:54:16 +0300

    Refactored AccountVerificationManager according to updatable JPA modifier. (Check previous commit to see what's changed)

--- src/main/java/com/hrms/business/concretes/AccountVerificationManager.java
+++ src/main/java/com/hrms/business/concretes/AccountVerificationManager.java
@@ -7,7 +7,6 @@ import com.hrms.core.utilities.results.Result;
 import com.hrms.core.utilities.results.SuccessResult;
 import com.hrms.dataAccess.abstracts.AccountVerificationDao;
 import com.hrms.entities.concretes.AccountVerification;
-import com.hrms.entities.concretes.Staff;
 import com.hrms.entities.concretes.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -26,7 +25,7 @@ public class AccountVerificationManager implements AccountVerificationService {
     @Override
     public Result createVerification(User user) {
         user.setAccountVerified(false);
-        AccountVerification verification = new AccountVerification(0, user, null, false);
+        AccountVerification verification = new AccountVerification(0, user, null);
         verificationDao.save(verification);
         return new SuccessResult();
     }
@@ -34,10 +33,7 @@ public class AccountVerificationManager implements AccountVerificationService {
     @Override
     public Result verify(AccountVerification verification) {
         AccountVerification verificationToUpdate = verificationDao.getReferenceById(verification.getId());
-        Staff reviewer = staffService.get(verification.getReviewer().getId()).getData();
-
-        verificationToUpdate.setReviewer(reviewer);
-        verificationToUpdate.setVerified(true);
+        verificationToUpdate.setReviewer(verification.getReviewer());
         verificationToUpdate.getUser().setAccountVerified(true);
         verificationDao.save(verificationToUpdate);
         return new SuccessResult(Messages.ACCOUNT_HAS_BEEN_VERIFIED);
